2c34ddf30ccf942cbce9e2b9c49147eef98e04c4
prateekparallel <prateekparallel@gmail.com>
2018-09-12 22:14:37 +0100

    Adding Student class

---
+++ StudentRegistrationDemo3/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationDemo3.Models
+{
+    public class Student
+    {
+        String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        String registrationNumber;
+
+        public String RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+
+
+    }
+}
