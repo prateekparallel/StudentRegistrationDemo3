42374dc6bdcdefd5a66e6949779f51a09dfc9516
prateekparallel <prateekparallel@gmail.com>
2018-09-12 22:22:46 +0100

    Adding PUT Controller class

---
+++ StudentRegistrationDemo3/Models/StudentUpdateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+using StudentRegistrationDemo3.Models;
+
+namespace StudentRegistrationDemo3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentUpdateController : Controller
+    {
+        // GET: api/<controller>
+        [HttpPut]
+        public JsonResult UpdateStudentRecord( Student stdn)
+        {
+            Console.WriteLine("In updateStudentRecord");
+            return Json(StudentRegistration.getInstance().UpdateStudent(stdn));
+        }
+    }
+}
